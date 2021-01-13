c95a431c2e7426bc477dc7e804a5b565f4a492a6
kutluhang <kutluhang@yahoo.com>
2021-01-13 21:31:13 +0100

    codingBookHomePage senaryolari ve testStep definition lari olusturuldu

---
+++ src/test/java/TestSteps/MyStepdefs.java
@@ -0,0 +1,52 @@
+package TestSteps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("User opens CodingBook web page")
+    public void userOpensCodingBookWebPage() {
+    }
+
+    @When("User checks if CodingBook logo is visible on the top left corner")
+    public void userChecksIfCodingBookLogoIsVisibleOnTheTopLeftCorner() {
+    }
+
+    @And("User clicks CodingBook logo")
+    public void userClicksCodingBookLogo() {
+    }
+
+    @Then("Check if user lands CodingBook homepage")
+    public void checkIfUserLandsCodingBookHomepage() {
+    }
+
+    @When("User checks if CodingBook LinkedIn icon is visible on the top right corner")
+    public void userChecksIfCodingBookLinkedInIconIsVisibleOnTheTopRightCorner() {
+    }
+
+    @And("User clicks CodingBook LinkedIn icon")
+    public void userClicksCodingBookLinkedInIcon() {
+    }
+
+    @Then("Check if user lands CodingBook LinkedIn page")
+    public void checkIfUserLandsCodingBookLinkedInPage() {
+    }
+
+    @Given("User lands to Courses menu on CodingBook web page")
+    public void userLandsToCoursesMenuOnCodingBookWebPage() {
+    }
+
+    @When("User moves mouse to Test Automation sub-menu under Courses menu")
+    public void userMovesMouseToTestAutomationSubMenuUnderCoursesMenu() {
+    }
+
+    @And("User clicks Test Automation sub-menu")
+    public void userClicksTestAutomationSubMenu() {
+    }
+
+    @Then("Check if user lands Test Automation page")
+    public void checkIfUserLandsTestAutomationPage() {
+    }
+}
